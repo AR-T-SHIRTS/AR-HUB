7f253c9ee0f98315f44f54919e1239ca853ca0d1
Pablo <alu0101103181@ull.edu.es>
2020-01-13 00:55:45 +0000

    SingIn, SingUp, email,password and confirm password format

--- src/javascript/firebase-auth.js
+++ src/javascript/firebase-auth.js
@@ -1,16 +1,61 @@
-var ref = new Firebase("https://ar-hub-6c9e7.firebaseapp.com/");
-
-var user = {
-    email: "malvarez@desarrolloweb.com",
-    password: "1234"
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB_cZ0r4c5wKQxPriRfra0__nA1_TEFW-M",
+    authDomain: "ar-hub-6c9e7.firebaseapp.com",
+    databaseURL: "https://ar-hub-6c9e7.firebaseio.com",
+    projectId: "ar-hub-6c9e7",
+    storageBucket: "ar-hub-6c9e7.appspot.com",
+    messagingSenderId: "652513003516",
+    appId: "1:652513003516:web:aed8b4063f4e8dbd6a5479"
 };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-ref.authWithPassword(user);
+//event listener
+document.getElementById('signup').addEventListener('submit', submitSignUp)
+document.getElementById('signin').addEventListener('submit', submitSignIn)
 
-ref.createUser(user, function (error) {
-    if (error) {
-        console.log(error.code);
-    } else {
-        console.log("Tu usuario se ha registrado");
-    }
-});
+function submitSignIn(e) {
+    e.preventDefault();
+
+    var email = getInputVal('email');
+    var password = getInputVal('password');
+    const auth = firebase.auth();
+
+    const promise = auth.signInWithEmailAndPassword(email, password);
+    promise
+    .then(user => console.log(user))
+    .catch(e=> console.log(e.message));
+}
+
+
+function submitSignUp(e) {
+    e.preventDefault();
+
+    var email = getInputVal('email');
+    var password = getInputVal('password');
+    const auth = firebase.auth();
+
+    const promise = auth.createUserWithEmailAndPassword(email, password);
+    promise.catch(e=> console.log(e.message));
+}
+
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
+
+//funciones de validacion de contrasena
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
