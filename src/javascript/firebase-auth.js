e230c9cff48de28fd73d28e4b2ee06de2879ce1e
Pablo Molina Martinez <alu0101103181@ull.edu.es>
2020-01-15 20:37:00 +0000

    event try

--- src/javascript/firebase-auth.js
+++ src/javascript/firebase-auth.js
@@ -6,11 +6,13 @@ var firebaseConfig = {
     projectId: "ar-hub-6c9e7",
     storageBucket: "ar-hub-6c9e7.appspot.com",
     messagingSenderId: "652513003516",
-    appId: "1:652513003516:web:aed8b4063f4e8dbd6a5479"
+    appId: "1:652513003516:web:aed8b4063f4e8dbd6a5479",
+    measurementId: "G-XPF7XDFX50"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 //event listener
 document.getElementById('signup').addEventListener('submit', submitSignUp)
@@ -23,14 +25,15 @@ function submitSignIn(e) {
     var password = getInputVal('passwords');
     const auth = firebase.auth();
 
+    firebase.analytics().logEvent('notification_received');
+
     auth.signOut().catch(e => console.log(e.message));
 
     const promise = auth.signInWithEmailAndPassword(email, password);
     promise
         .then(e => window.location = 'models_and_markers.html')
         .catch(e => alert(e.message));
-        
-    firebase.analytics().logEvent('intento de inicio de sesion');
+
 }
 
 function submitSignUp(e) {
